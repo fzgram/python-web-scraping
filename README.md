d2c2c44201d3e1e2151f7f02da9ae3fb9a2fde87
zlpest123 <fzgram8@gmail.com>
2024-09-20 17:36:51 +0300

    auto downloading

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # Web Scraping with Python Selenium
 
+## Installing Python if needed
+
+1. Download Python Installer:
+Go to the official Python website: python.org/downloads.
+Download the latest version of Python for Windows.
+
+2. Run the Installer:
+Double-click the downloaded .exe file to launch the installer.
+IMPORTANT: Check the box that says "Add Python to PATH" before clicking Install.
+
+3. Verify Installation:
+Open Command Prompt.
+Type python --version and press Enter. If installed correctly, it will display the installed Python version.
+
 ## Installing Selenium
 
 1. Create a virtual environment:
@@ -13,3 +27,19 @@ python -m venv .env
 ```sh
 pip install selenium
 ```
+
+## Running Python Script
+1. Open 'src' folder
+
+2. Set last page number
+
+Open 'requirements.txt' and set the page number
+
+3. Open Command Prompt in 'src' folder
+
+4. Run Python Script
+
+```sh
+python main.py
+```
+
--- src/main.py
+++ src/main.py
@@ -1,37 +1,104 @@
 from selenium.webdriver import Chrome, ChromeOptions
 from selenium.webdriver.common.by import By
 
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
+# from selenium.common.exceptions import StaleElementReferenceException
 
-def get_data(url: str) -> list:
+import os
+import time
+
+def is_download_complete(download_dir):
+    while True:
+        files = os.listdir(download_dir)
+        if not files:  # No files in the directory
+            time.sleep(1)  # Wait before checking again
+            continue
+        for filename in files:
+            if filename.endswith('.crdownload'):  # Check for incomplete downloads
+                break
+        else:
+            # No .crdownload files found, download is complete
+            return True
+        time.sleep(1)  # Wait before checking again
+
+def get_data(url: str):
     browser_options = ChromeOptions()
     browser_options.headless = True
     
+    download_dir = r"C:\Users\h\Downloads"
+    print('your download dir: ', download_dir)
+
+    browser_options.add_experimental_option("prefs", {
+        "download.default_directory": download_dir,  # Set the download directory
+        "download.prompt_for_download": False,
+        "directory_upgrade": True,
+        "safebrowsing.enabled": True
+    })
+    
     driver = Chrome(options=browser_options)
     driver.get(url)
+    
+    # iframe = driver.find_element(By.TAG_NAME, "iframe")
+    # driver.switch_to.frame(iframe)
 
-    element = driver.find_element(By.LINK_TEXT, "Humor")
-    element.click()
+    # wait = WebDriverWait(driver, 10)
+    # element = wait.until(EC.element_to_be_clickable((By.ID, "_idJsp2:_idJsp11")))
+    
+    # print(driver.page_source)
+    
+    tab = driver.find_element(By.CSS_SELECTOR, 'a#_idJsp5\\:_idJsp14')
+    tab.click()
+    download_btns_count = len(driver.find_elements(By.LINK_TEXT, "Изтегли броя"))
+    
+    text = open("./requirements.txt")
+    page_count = text.readline().strip().split('=')[1].strip()
+    page_count = int(page_count)
+    print('page counts: ', page_count)
+    text.close()
+    
+    i = 0
+    while i < page_count:
+        for j in range(download_btns_count):
+            #  because the element is Re-located
+            print(f"start {i*download_btns_count + j +1}")
+            download_btns = driver.find_elements(By.LINK_TEXT, "Изтегли броя")
+            download_btns[j].click()
+            print("btn cliked")
+            
+            popup = driver.find_element(By.PARTIAL_LINK_TEXT, '.rtf')
+            popup.click()
+            print("popup cliked")
+            
+            # wait until download has completed.
+            is_download_complete(download_dir)
+            
+            close_btn = driver.find_element(By.CSS_SELECTOR, "img[src='/DVWeb/img/exit.gif'][onclick='hideDownloadModalPanel()']")
+            close_btn.click()
+            print(f"end {i*download_btns_count + j +1}")
+            
+        i += 1
+        next_btn = driver.find_element(By.LINK_TEXT, "Следваща.. »")
+        next_btn.click()
+    
+    # for book in books:
+    #     title = book.find_element(By.CSS_SELECTOR, "h3 > a")
+    #     price = book.find_element(By.CSS_SELECTOR, ".price_color")
+    #     stock = book.find_element(By.CSS_SELECTOR, ".instock.availability")
+    #     book_item = {
+    #         'title': title.get_attribute("title"),
+    #         'price': price.text,
+    #         'stock': stock.text
+    #     }
+    #     data.append(book_item)
 
-    books = driver.find_elements(By.CSS_SELECTOR, ".product_pod")
-    data = []
-    for book in books:
-        title = book.find_element(By.CSS_SELECTOR, "h3 > a")
-        price = book.find_element(By.CSS_SELECTOR, ".price_color")
-        stock = book.find_element(By.CSS_SELECTOR, ".instock.availability")
-        book_item = {
-            'title': title.get_attribute("title"),
-            'price': price.text,
-            'stock': stock.text
-        }
-        data.append(book_item)
-
+    time.sleep(10)
     driver.quit()
-    return data
 
 
 def main():
-    data = get_data("https://books.toscrape.com/")
-    print(data)
+    get_data("https://dv.parliament.bg/DVWeb/broeveList.faces")
+    print("OK")
 
 
 if __name__ == '__main__':
